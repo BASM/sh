3a61a41c20ef17eec0f5a49a6f82a6dc72151b35
Leonid Muravjev <asm@asm.pp.ru>
2015-01-16 10:47:51 +0300

    Board.yml parces

--- boards/switch/main.py
+++ boards/switch/main.py
@@ -1,15 +1,45 @@
 import yaml
+#import ./pios
 
-f = open("board.yml")
-yml = yaml.safe_load(f)
-f.close()
+class PIO:
+    def __init__(self,pio,type):
+        self.pio=pio
+        self.name=pio["TYPE"]
+        self.type=type
 
-name = yml["NAME"]
+    def print(s):
+        print("Name: %s, type: %s"%(s.name,s.type))
+        print("PIOS: %s\n"%s.pio["PINS"])
 
-print("Board name '%s'"%(name))
+class IC:
+    def __init__(s,ic):
+        s.type=ic["TYPE"]
+        s.pios=[]
+        icpio=ic["PIO"]
+        for pio in icpio:
+            cp = PIO(icpio[pio],pio)
+            s.pios+=[cp]
 
-for i in yml["IC"]:
-    print (i)
+    def print(s):
+        print ("IC type: %s"%s.type)
+        for p in s.pios:
+            p.print()
 
-print("=============\n")
-print(yml)
+class BOARD:
+    def __init__(s,fname):
+        f = open("board.yml")
+        s.yml = yaml.safe_load(f)
+        f.close()
+        s.name = s.yml["NAME"]
+        s.ic=[]
+
+        for i in s.yml["IC"]:
+            s.ic+=[IC(i)]
+
+    def print(s):
+        print("Board name '%s'"%(s.name))
+        for i in s.ic:
+            i.print()
+
+brd=BOARD("board.yml")
+brd.print()
