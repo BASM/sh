01363c6340f8a1b5331e9991fc29bcf7be2041d1
Maria Levshinovskaya <masa.levs@gmail.com>
2015-03-05 22:10:04 +0300

    Relay in use

--- app/sproto/src/avr.cc
+++ app/sproto/src/avr.cc
@@ -7,7 +7,7 @@ MCU_PROTO::MCU_PROTO () {
   wdt_disable();
   cli();
   l1.init(&DDRC, &PORTC, 0);
-  r1.init(&DDRC, &PORTC, 0);
+  r1.init(&DDRB, &PORTB, 4);
   r2.init(&DDRC, &PORTC, 0);
   b1.init(&DDRD, &PORTD, &PIND, 3);
   b2.init(&DDRD, &PORTD, &PIND, 4);
--- app/sproto/src/main.cc
+++ app/sproto/src/main.cc
@@ -8,14 +8,14 @@ int main(void) {
   //printf("HELLO SPROTO\n");
 
   ic.l1.set();
-  ic.sleep_s(1);
-  ic.l1.clr();
 
   while (1) {
     if (ic.b1.isOn() != ic.b2.isOn()) {
-      ic.l1.set();
-    } else {
       ic.l1.clr();
+      ic.r1.set();
+    } else {
+      ic.l1.set();
+      ic.r1.clr();
     }
     ic.sleep_ms(100);
   }
